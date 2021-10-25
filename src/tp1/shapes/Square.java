ad7fed535f6ce93b79fb26cace122a6e032ab8de
ManiAzza <84026665+ManiAzza@users.noreply.github.com>
2021-10-25 08:53:24 +0100

    Create Square.java

---
+++ src/tp1/shapes/Square.java
@@ -0,0 +1,17 @@
+package shapes;
+
+public class Square {
+	//attributes
+		public int a ; 
+
+		
+	    //methods
+	    public void perimeter(int a ){
+	    
+	        System.out.println("Perimeter : " +(4*a) );
+	    }
+
+	    public void surface(int a ){
+	        System.out.println("Surface : "+ (a*a));
+	    }
+}
