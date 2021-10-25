da31942c49e49d4dea639b753c677e54c05bb296
ManiAzza <84026665+ManiAzza@users.noreply.github.com>
2021-10-25 08:49:39 +0100

    Update Main.java

--- src/tp1/shapes/Main.java
+++ src/tp1/shapes/Main.java
@@ -2,6 +2,14 @@ package tp1.shapes;
 
 public class Main {
     public static void main(String[] args) {
+        
+        Triangle A = new Triangle() ;
+    	A.perimeter(5, 2, 3);
+    	A.surface(50, 20, 33);
+    	
+    	Rectangle B = new Rectangle() ; 
+    	B.perimeter(4, 5);
+    	B.surface(5, 6);
         // if I want to create a new shape (for example square)
         // how can I force the implementation of perimeter and surface on that shape ?
     }
