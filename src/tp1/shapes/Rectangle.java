402e7a0f1bb572d347203c706e36f10307017262
ManiAzza <84026665+ManiAzza@users.noreply.github.com>
2021-10-25 08:48:20 +0100

    Update Rectangle.java

--- src/tp1/shapes/Rectangle.java
+++ src/tp1/shapes/Rectangle.java
@@ -1,14 +1,18 @@
-package tp1.shapes;
+package shapes;
 
 public class Rectangle {
     //attributes
-
+	public int a ; 
+	public int b ;
+	
     //methods
-    public void perimeter(){
-        System.out.println("Perimeter : ");
+    public void perimeter(int a , int b ){
+    	int p = (a+b)*2 ; 
+    	
+        System.out.println("Perimeter : " +p );
     }
 
-    public void surface(){
-        System.out.println("Perimeter : ");
+    public void surface(int a , int b ){
+        System.out.println("Surface : "+ (a*b));
     }
 }
